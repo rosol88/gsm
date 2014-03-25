79a132d23644fec34da54e5ff4ff7b900521cd7a
Stave jobs <sj@apple.com>
2014-03-25 11:09:48 +0100

    Testy

--- gsm/src/gsm/model/Phone.java
+++ gsm/src/gsm/model/Phone.java
@@ -5,98 +5,77 @@ import gsm.exeption.NoNetworkException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Phone
-{
+public class Phone {
 
-    private String number;
+	private String number;
 
-    private Network network;
+	private Network network;
 
-    private List<Message> messages = new ArrayList<Message>();
+	private List<Message> messages = new ArrayList<Message>();
 
-    private Call call;
+	private Call call;
 
-    public Phone( String number, Plan plan )
-    {
-        super();
-        this.number = number;
-        this.plan = plan;
-        getOperator().addPhone(this);
-    }
+	public Phone(String number, Plan plan) {
+		super();
+		this.number = number;
+		this.plan = plan;
+		getOperator().addPhone(this);
+	}
 
-    private Plan plan;
+	private Plan plan;
 
-    public Operator getOperator(){
-    	return plan.getOperator();
-    }
-    public String getNumber()
-    {
-        return number;
-    }
+	public Operator getOperator() {
+		return plan.getOperator();
+	}
 
-    public void setNumber( String number )
-    {
-        this.number = number;
-    }
+	public String getNumber() {
+		return number;
+	}
 
-    public Network getNetwork()
-    {
-        return network;
-    }
+	public Network getNetwork() {
+		return network;
+	}
 
-    public void setNetwork( Network network )
-    {
-        this.network = network;
-        network.addPhone(this);
-    }
+	public void setNetwork(Network network) {
+		this.network = network;
+		network.addPhone(this);
+	}
 
-    public Plan getPlan()
-    {
-        return plan;
-    }
+	public Plan getPlan() {
+		return plan;
+	}
 
-    public void setPlan( Plan plan )
-    {
-        this.plan = plan;
-    }
+	public void sendText(String number, String text) {
+		checkNetwork();
+		getNetwork().sendText(getNumber(), number, text);
+	}
 
-    public void sendText( String number, String text )
-    {
-        checkNetwork();
-        getNetwork().sendText( getNumber(), number, text );
-    }
+	private void checkNetwork() {
+		if (getNetwork() == null)
+			throw new NoNetworkException();
 
-    private void checkNetwork()
-    {
-        if ( getNetwork() == null )
-            throw new NoNetworkException();
+	}
 
-    }
+	public void startCall(String number) {
+		call = getNetwork().call(getNumber(), number);
+	}
 
-    public void startCall( String number )
-    {
-        call = getNetwork().call( getNumber(), number );
-    }
+	public void stopCall() {
+		call.stop();
+	}
 
-    public void stopCall()
-    {
-        call.stop();
-    }
+	public void sendData(Object data) {
 
-    public void sendData( Object data )
-    {
+	}
 
-    }
+	public void getData() {
 
-    public void getData()
-    {
+	}
 
-    }
+	public void receiveText(Message message) {
+		messages.add(message);
+	}
 
-    public void receiveText( Message message )
-    {
-        messages.add( message );
-    }
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -104,6 +83,7 @@ public class Phone
 		result = prime * result + ((number == null) ? 0 : number.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -120,5 +100,5 @@ public class Phone
 			return false;
 		return true;
 	}
-    
+
 }
---
+++ gsm/test/gsm/unit/PhoneTest.java
@@ -0,0 +1,89 @@
+package gsm.unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import gsm.exeption.NoNetworkException;
+import gsm.model.Call;
+import gsm.model.Network;
+import gsm.model.Operator;
+import gsm.model.Phone;
+import gsm.model.Plan;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class PhoneTest {
+
+	@Mock
+	Plan plan;
+	@Mock
+	Plan plan2;
+	@Mock
+	Operator operator;
+
+	@Mock
+	Network network;
+
+	@Mock
+	Call call;
+
+	@Mock
+	Phone testPhone;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		when(plan.getOperator()).thenReturn(operator);
+		when(plan2.getOperator()).thenReturn(operator);
+	}
+
+	@Test(expected = NoNetworkException.class)
+	public void sendTextNoNetwork() {
+		Phone phone = new Phone("123", plan);
+		phone.sendText("321", "abc");
+	}
+
+	@Test
+	public void sendText() {
+		Phone phone = new Phone("123", plan);
+		phone.setNetwork(network);
+		phone.sendText("321", "abc");
+		verify(network).sendText("123", "321", "abc");
+	}
+
+	@Test
+	public void receiveText() {
+
+		// when(testPhone.sendText("321", "abc"));
+		testPhone.sendText("321", "abc");
+
+		Phone phone2 = new Phone("321", plan);
+		phone2.setNetwork(network);
+
+		testPhone.sendText("321", "abc");
+
+		// verify(phone2).receiveText(message);
+	}
+
+	@Test
+	public void callTest() {
+		Phone phone = new Phone("123", plan);
+		phone.setNetwork(network);
+		when(network.call("123", "321")).thenReturn(call);
+		phone.startCall("321");
+		phone.stopCall();
+		verify(network).call("123", "321");
+		verify(call).stop();
+	}
+
+	@Test
+	public void equalsTest() {
+		Phone phone = new Phone("123", plan);
+		Phone phone2 = new Phone("123", plan2);
+		assertEquals(phone, phone2);
+	}
+
+}
